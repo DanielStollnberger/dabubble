47b5f9904697dbd7b3f34ebb4a77e106b5f92e7b
Daniel Stollnberger <daniiistolliii@gmail.com>
2026-03-05 02:03:37 +0100

    first commit + setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,8 +4,22 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD_MYUxj2sL0WkwjI5K55WpM7izu8ZOwFE",
+  authDomain: "dabubble-93439.firebaseapp.com",
+  projectId: "dabubble-93439",
+  storageBucket: "dabubble-93439.firebasestorage.app",
+  messagingSenderId: "275750981191",
+  appId: "1:275750981191:web:e56546c68aa03b26872b62",
+  measurementId: "G-V5HZ2VQP20"
+};
+
 export const appConfig: ApplicationConfig = {
-  providers: [
+  providers: [ provideFirebaseApp(() => initializeApp( firebaseConfig )),
+    provideFirestore(() => getFirestore()),
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes), provideClientHydration(withEventReplay())
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,22 @@
-import { Component, signal } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { collectionData, Firestore } from '@angular/fire/firestore';
 import { RouterOutlet } from '@angular/router';
+import { collection } from 'firebase/firestore';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    AsyncPipe
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('dabubble');
+
+  firestore = inject(Firestore);
+  itemCollection = collection(this.firestore, 'items');
+  item$ = collectionData(this.itemCollection);
 }
